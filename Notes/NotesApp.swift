edefa91d9c79d1ebcd629de31a29874850c3def2
Justin Chester <slict43@icloud.com>
2022-01-04 03:02:33 -0500

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Justin Chester on 2022-01-04.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
